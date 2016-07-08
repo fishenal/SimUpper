92badc508e12cdee65ceb74f4d252d9fddf693de
yudonghan <yudonghan@baidu.com>
2016-07-08 17:22:37 +0800

    classy
    classy

---
+++ src/module/List.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="up-list">
+    <div class="up-item" v-for="(index, item) in list">
+      <!-- <div>
+          <h1>{{item.title}} </h1>
+
+      </div>
+      <div class="up-detail">
+          <span>一个{{item.style.label}}的{{item.type.label}}视频</span>
+          <span>质量： {{item.quality.label}}</span>
+          <span>完成度：{{item.finishStatus }} %</span>
+          <span>视频评分：{{item.score}}</span>
+          <span>视频持续天数：{{item.day}}</span>
+          <span>play times: {{item.playtime}}</span>
+          <span>like: {{item.like}}</span>
+          <span>commits: {{item.commits.length}}</span>
+
+          <ul>
+            <li v-for="cont in item.commits" track-by="$index">
+              {{ cont.cont }}
+            </li>
+          </ul>
+      </div>
+      <div class="dt-button" v-if="item.finishStatus===100&&!item.online" @click="publish(index, item)">
+          <a href="#">publish</a>
+      </div>
+      <div class="dt-button" v-if="item.finishStatus!==100&&!item.online" @click="continuee(index, item)">
+          <a href="#">continue make</a>
+      </div>
+      <div class="dt-button" @click="remove(index, item)">
+          <a href="#">remove</a>
+      </div>
+      <div v-if="item.online">online!</div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+        ids: [],
+        list: []
+        // list: itemStore.fetch()
+    }
+  },
+  methods: {
+    // continuee: function (index, item) {
+    //   this.$emit('continuemake', index, item)
+    // },
+    // publish: function (index, item) {
+    //   this.$emit('publish', index, item)
+    // },
+    // remove: function (index, item) {
+    //   this.$emit('remove', index, item)
+    // }
+  }
+  // watch: {
+  //     list: {
+  //       handler: function () {
+  //           this.$emit('update', this.list)
+  //       },
+  //       deep: true
+  //     }
+  // },
+  // events: {
+  //   'refresh-list': function () {
+  //       this.list = itemStore.fetch()
+  //   }
+  // }
+}
+</script>
+
+<style scoped>
+    .up-item {
+        border-bottom: 2px solid;
+        margin-bottom: 50px;
+    }
+    .up-detail span {
+        margin-right: 15px;
+    }
+</style>
---
+++ src/module/My.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="dt-box">
+    <div class="dt-item-4">
+        <img src="../img/avatar.jpg">
+    </div>
+    <div class="dt-item-8">
+        <ul class="dt-list-3 dt-light">
+          <li>username: {{username}}</li>
+          <li>follower: {{follower}}</li>
+          <li>publish: {{publish}}</li>
+          <li>power: {{power}}</li>
+        </ul>
+        <h2>技能</h2>
+        <ul class="dt-list-3 dt-light">
+          <li v-for="item in abilities">
+            {{ item.label }} : {{ item.abi }}
+          </li>
+        </ul>   
+    </div>  
+  </div>
+  <div class="dt-box">
+      <div class="dt-item-8">
+          <h3 class="dt-light">day {{day}}</h3>
+      </div>
+      <div class="dt-item-2">
+          <div class="dt-button" @:click="nextDay">
+              <a href="#">next</a>
+          </div>
+      </div>  
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+        username: '',
+        follower: 0,
+        publish: 0,
+        power: 0,
+        abilities: {},
+        day: 1
+    }
+  },
+  methods: {
+    // continuee: function (index, item) {
+    //   this.$emit('continuemake', index, item)
+    // },
+    // publish: function (index, item) {
+    //   this.$emit('publish', index, item)
+    // },
+    // remove: function (index, item) {
+    //   this.$emit('remove', index, item)
+    // }
+    save: function () {
+
+    },
+    fetch: function () {
+
+    }
+  }
+  // watch: {
+  //     list: {
+  //       handler: function () {
+  //           this.$emit('update', this.list)
+  //       },
+  //       deep: true
+  //     }
+  // },
+  // events: {
+  //   'refresh-list': function () {
+  //       this.list = itemStore.fetch()
+  //   }
+  // }
+}
+</script>
+
+<style scoped>
+    .up-item {
+        border-bottom: 2px solid;
+        margin-bottom: 50px;
+    }
+    .up-detail span {
+        margin-right: 15px;
+    }
+</style>
